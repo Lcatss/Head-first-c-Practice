a58d2af1317f0945f9d694f87cb997ab64adb4d3
Lcatss <born97@vip.qq.com>
2017-10-22 21:13:46 +0800

    BAll

---
+++ Ball/Ball/Pitcher.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace WindowsFormsApplication1
+{
+    class Pitcher
+    {
+        public Pitcher(Ball ball)
+        {
+            ball.BallInPlay += new EventHandler(ball_BallInPlay);
+        }
+
+
+        void ball_BallInPlay(object sender, EventArgs e)
+        {
+            if (e is BallEventArgs)
+            {
+                BallEventArgs ballEventArgs = e as BallEventArgs;
+                if ((ballEventArgs.Distance < 95) && (ballEventArgs.Trajectory < 60))
+                    CatchBall();
+                else
+                    CoverFirstBase();
+            }
+        }
+    }
+}
