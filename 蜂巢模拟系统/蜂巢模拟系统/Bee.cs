14d57334014e58c2e20173ddd51368ab92b5317e
Lcatss <born97@vip.qq.com>
2017-10-26 21:17:55 +0800

    up

--- 蜂巢模拟系统/蜂巢模拟系统/Bee.cs
+++ 蜂巢模拟系统/蜂巢模拟系统/Bee.cs
@@ -16,6 +16,7 @@ namespace WindowsFormsApplication1
         public int Age { get;private set; }
         public bool InsideHive { get;private set; }
         public double NectarCollected { get;private set; }
+        public BeeState CurrentState { get;private set; }
 
         private Point location;
         public Point Location { get { return location; } }
@@ -31,11 +32,59 @@ namespace WindowsFormsApplication1
             this.InsideHive = true;
             this.destinationFlower = null;
             this.NectarCollected = 0;
+            this.CurrentState = BeeState.Idle;
         }
 
         public void Go(Random random)
         {
             Age++;
+            switch (CurrentState)
+            {
+                case BeeState.Idle:
+                    if (Age > CareerSpan)
+                        CurrentState = BeeState.Retired;
+                    else
+                    {
+                        //What we do when idle
+                    }
+                    break;
+                case BeeState.FlyingToFlower:
+                    //move towards flower
+                    break;
+                case BeeState.GatheringNectar:
+                    double nectar = destinationFlower.HarvestNectar();
+                    if (nectar > 0)
+                        NectarCollected += nectar;
+                    else
+                        CurrentState = BeeState.ReturningToHive;
+                    break;
+                case BeeState.ReturningToHive:
+                    if (!InsideHive)
+                    {
+                        //move towards hive
+                    }
+                    else
+                    {
+                        //what we do in the  Hive
+                    }
+                    break;
+                case BeeState.MakingHoney:
+                    if (NectarCollected < 0.5)
+                    {
+                        NectarCollected = 0;
+                        CurrentState = BeeState.Idle;
+                    }
+                    else
+                    {
+                        //make honey from using nectar
+                    }
+                    break;
+                case BeeState.Retired:
+                    //do nothing
+                    break;
+            }
+        
+
         }
 
         private bool MoveTowardsLocation(Point destination)
